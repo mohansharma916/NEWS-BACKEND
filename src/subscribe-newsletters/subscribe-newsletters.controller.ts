97ce704917d7e7b9aec0aceb439c09d5e9125d55
tanzeem741 <tanzeems741@gmail.com>
2025-12-01 20:01:50 +0530

    add authentication to get api's

--- src/subscribe-newsletters/subscribe-newsletters.controller.ts
+++ src/subscribe-newsletters/subscribe-newsletters.controller.ts
@@ -1,22 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UseGuards } from '@nestjs/common';
 import { SubscribeNewslettersService } from './subscribe-newsletters.service';
 import { CreateSubscribeNewsletterDto } from './dto/create-subscribe-newsletter.dto';
+import { RolesGuard } from 'src/auth/guards/role.guards';
+import { Role } from 'generated/prisma/client';
+import { Roles } from 'src/auth/decorators/role.decorators';
+import { AuthGuard } from '@nestjs/passport';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('subscribe-newsletters')
 export class SubscribeNewslettersController {
   constructor(private readonly subscribeNewslettersService: SubscribeNewslettersService) {}
 
   @Post()
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
   create(@Body() createSubscribeNewsletterDto: CreateSubscribeNewsletterDto) {
     return this.subscribeNewslettersService.create(createSubscribeNewsletterDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.SUPER_ADMIN, Role.EDITOR)
   findAll() {
     return this.subscribeNewslettersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.SUPER_ADMIN, Role.EDITOR)
   findOne(@Param('id') id: string) {
     return this.subscribeNewslettersService.findOne(id);
   }
